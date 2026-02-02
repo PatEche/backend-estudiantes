1753177bf8445f25abe360f49318f695e9550ead
PatEche <echeverriaple@gmail.com>
2026-02-02 17:07:08 -0300

    feat: implementacion de entidades usuario y estudiante

---
+++ src/main/java/com/backend/estudiantes/model/Usuario.java
@@ -0,0 +1,34 @@
+package com.backend.estudiantes.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @Column(nullable = false)
+    private String apellido;
+
+    @Column(name="email",unique = true,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "boolean default true")
+    private boolean activo = true;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+}
